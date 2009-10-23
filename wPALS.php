873c66355b5b25cc52d4d5becc33a22dd30b1ff8
Chip Caddell <c@chipcaddell.com>
2009-10-22 20:56:48 -0400

    Created master plugin file.

---
+++ wPALS.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package wPALS
+ * @author Chip Caddell
+ * @version .1
+ */
+/*
+Plugin Name: Word Press Access List Service
+Plugin URI: 
+Description: This pluging offers blogs an easy way of restricting and allowing access to blog content.
+Author: Chip Caddell
+Version: .1
+Author URI: 
+*/
+
+
+?>
